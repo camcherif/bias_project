9d886ebb6c9533b846878798caf4c5db5dd81fa7
Alyssa Hajj Assaf <alyssa.hajjassaf@mail.mcgill.ca>
2022-03-25 17:42:37 -0400

    Update yahoo_bias.py
    Included location more appropriately and correcting the if statement to check the status code

--- yahoo_bias.py
+++ yahoo_bias.py
@@ -1,20 +1,18 @@
 import requests
 import re
 import json
+import pandas as pd, pycountry
 
 def yahoo_autosugg(feature, loca):
 
   #so basically this is the url I found to get yahoo autosuggestion for women
   #there's no api available and this is the only feasible solution I found lmao
   #we just need to replace women in the url by the feature we're actually looking for and it works great
-  locas = ["fr", "ca", "en", "us", "ar", "in"]
-  if loca not in locas:
-      loca = "ca" #default value
   url = "https://"+loca+".search.yahoo.com/sugg/gossip/gossip-"+loca+"-ura/?pq="+feature+"%20are&command="+feature+"%20are&t_stmp=1648056743&callback=YAHOO.SA.apps%5B0%5D.cb.sacb0&l=1&bm=3&output=sd1&nresults=10&geoid=23897142&.crumb=zDg1NmBdWJ0&f=1&appid=ca.search.yahoo.com&bck=9kb8gcdh39es9%26b%3D3%26s%3D1m&csrcpvid=InOFpTEwLjGaLRBjYjS7iQLiNzQuNQAAAAD5R1sM&vtestid=&mtestid=24576%3DD1114&spaceId=2114721002"
 
   r = requests.get(url)
   
-  if r>300:
+  if int(r.status_code)>300:
         return []
 
   #looks for the autosuggestions 
@@ -32,6 +30,8 @@ def yahoo_autosugg(feature, loca):
 
 var = input("Enter your query: ")
 var_space = var.replace(' ', '%20')
-loca = "ca"
-print("autosuggestion results from "+loca)
-print(yahoo_autosugg(var_space, loca))
+country = input("Enter name of country of choice:")
+country = pycountry.countries.get(name=country).alpha_2
+print((str(country)).lower())
+print("autosuggestion results from "+country)
+print(yahoo_autosugg(var_space, country))
