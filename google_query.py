342be58d152927c9b7b25ce01bea42079c06fa8d
camcherif <cameron.cherif@outlook.com>
2022-03-25 14:12:28 -0400

    Added AR -Cam

--- google_query.py
+++ google_query.py
@@ -2,8 +2,8 @@ import requests, json
 
 var = input("Enter your query: ")
 var_space = var.replace(' ', '+')
-countries = ["FR","GB","US","CA","ES","ZA","IN"]; 
-URL = "http://suggestqueries.google.com/complete/search?client=firefox&q=" + var_space + "&hl=" + countries[0]
+countries = ["FR","GB","US","CA","ES","ZA","IN", "AR"]; 
+URL = "http://suggestqueries.google.com/complete/search?client=firefox&q=" + var_space + "&hl=" + countries[0] 
 headers = {'User-agent':'Mozilla/5.0'}
 response = requests.get(URL, headers=headers)
 result = json.loads(response.content.decode('utf-8'))[1][1:]
