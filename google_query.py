2c262e7c27ed0ff39b4ae149fbc11e8afd5c4867
camcherif <cameron.cherif@outlook.com>
2022-03-25 13:35:38 -0400

    Can do localised search FR, GB, US, CA, ES, ZA, IN

--- google_query.py
+++ google_query.py
@@ -2,9 +2,8 @@ import requests, json
 
 var = input("Enter your query: ")
 var_space = var.replace(' ', '+')
-print(var)
-
-URL = "http://suggestqueries.google.com/complete/search?client=firefox&q=" + var_space
+countries = ["FR","GB","US","CA","ES","ZA","IN"]; 
+URL = "http://suggestqueries.google.com/complete/search?client=firefox&q=" + var_space + "&hl=" + countries[0]
 headers = {'User-agent':'Mozilla/5.0'}
 response = requests.get(URL, headers=headers)
 result = json.loads(response.content.decode('utf-8'))[1][1:]
