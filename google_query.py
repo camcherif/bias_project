be3a12929dd803e803dd28446edb4b340945b6af
camcherif <cameron.cherif@outlook.com>
2022-03-25 13:14:57 -0400

    Can now enter queries containing spaces -Cam

--- google_query.py
+++ google_query.py
@@ -1,9 +1,10 @@
 import requests, json
 
 var = input("Enter your query: ")
+var_space = var.replace(' ', '+')
 print(var)
 
-URL = "http://suggestqueries.google.com/complete/search?client=firefox&q=" + var
+URL = "http://suggestqueries.google.com/complete/search?client=firefox&q=" + var_space
 headers = {'User-agent':'Mozilla/5.0'}
 response = requests.get(URL, headers=headers)
 result = json.loads(response.content.decode('utf-8'))[1][1:]
