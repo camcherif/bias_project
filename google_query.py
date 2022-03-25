f5350145a5a74799240e9103c82de6fd0b227fee
camcherif <cameron.cherif@outlook.com>
2022-03-25 13:01:05 -0400

    Cleaned up the code so it doesn't look like a script kiddie wrote it -Cam

--- google_query.py
+++ google_query.py
@@ -1,14 +1,14 @@
 import requests, json
 
 var = input("Enter your query: ")
+print(var)
 
 URL = "http://suggestqueries.google.com/complete/search?client=firefox&q=" + var
 headers = {'User-agent':'Mozilla/5.0'}
 response = requests.get(URL, headers=headers)
-result = json.loads(response.content.decode('utf-8'))
-i=1
-result = result[1]
-for query in result[1:]:
-    result[i] = query.replace(result[0] + ' ', '')
+result = json.loads(response.content.decode('utf-8'))[1][1:]
+i=0
+for query in result:
+    result[i] = query.replace(var + ' ', '')
     i+=1
-print(result[1:])
+print(result)
