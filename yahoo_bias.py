9881386626cc2c26f55b82e12ade45e1afce09c0
Alyssa Hajj Assaf <alyssa.hajjassaf@mail.mcgill.ca>
2022-03-25 15:16:14 -0400

    Update yahoo_bias.py
    added an array of different localizations cause it's swag

--- yahoo_bias.py
+++ yahoo_bias.py
@@ -2,12 +2,15 @@ import requests
 import re
 import json
 
-def yahoo_autosugg(feature):
+def yahoo_autosugg(feature, loca):
 
   #so basically this is the url I found to get yahoo autosuggestion for women
   #there's no api available and this is the only feasible solution I found lmao
   #we just need to replace women in the url by the feature we're actually looking for and it works great
-  url = "https://ca.search.yahoo.com/sugg/gossip/gossip-ca-ura/?pq="+feature+"%20are&command="+feature+"%20are&t_stmp=1648056743&callback=YAHOO.SA.apps%5B0%5D.cb.sacb0&l=1&bm=3&output=sd1&nresults=10&geoid=23897142&.crumb=zDg1NmBdWJ0&f=1&appid=ca.search.yahoo.com&bck=9kb8gcdh39es9%26b%3D3%26s%3D1m&csrcpvid=InOFpTEwLjGaLRBjYjS7iQLiNzQuNQAAAAD5R1sM&vtestid=&mtestid=24576%3DD1114&spaceId=2114721002"
+  locas = ["fr", "ca", "en", "us", "ar", "in"]
+  if loca not in locas:
+      loca = "ca" #default value
+  url = "https://"+loca+".search.yahoo.com/sugg/gossip/gossip-"+loca+"-ura/?pq="+feature+"%20are&command="+feature+"%20are&t_stmp=1648056743&callback=YAHOO.SA.apps%5B0%5D.cb.sacb0&l=1&bm=3&output=sd1&nresults=10&geoid=23897142&.crumb=zDg1NmBdWJ0&f=1&appid=ca.search.yahoo.com&bck=9kb8gcdh39es9%26b%3D3%26s%3D1m&csrcpvid=InOFpTEwLjGaLRBjYjS7iQLiNzQuNQAAAAD5R1sM&vtestid=&mtestid=24576%3DD1114&spaceId=2114721002"
 
   r = requests.get(url)
   
@@ -29,4 +32,6 @@ def yahoo_autosugg(feature):
 
 var = input("Enter your query: ")
 var_space = var.replace(' ', '%20')
-print(yahoo_autosugg(var_space))
+loca = "ca"
+print("autosuggestion results from "+loca)
+print(yahoo_autosugg(var_space, loca))
