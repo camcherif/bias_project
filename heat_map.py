9d5fc6af21a1229a53f5d7dc454d9092ad4b21d0
camcherif <cameron.cherif@outlook.com>
2022-04-17 15:47:50 -0400

    finished, going to implement scores

--- heat_map.py
+++ heat_map.py
@@ -1,4 +1,24 @@
-import geopandas as gpd, pandas as pd, matplotlib as plt, geoplot as gplot, mapclassify
+import geopandas as gpd, pandas as pd, matplotlib as plt, geoplot as gplot, mapclassify, pycountry, pandas as pd
 
-world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
-world = world.drop(columns=['gdp_md_est', "pop_est", "iso_a3", "continent"])
+
+##to remove once implemented with mother script
+countries = ['Canada', 'United States', 'Mexico', 'Singapore', 'New Zealand', 'Australia', 'France', 'United Kingdom', 'Japan', 'South Africa']
+
+def heat_map(countries, google_score, yahoo_score):
+    world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+    world = world.drop(columns=['gdp_md_est', "pop_est", "iso_a3", "continent"])
+    world = world.loc[world['name'].isin(countries)]
+    world['alpha_2']=world['name'].apply(lambda x: pycountry.countries.get(name=x).alpha_2)
+
+
+    fig, ax = plt.subplots(figsize = (10,4), facecolor = plt.cm.Blues(.2))
+    fig.suptitle('Country ',
+                fontsize = 'xx-large',  
+                fontweight = 'bold')
+    ax.set_facecolor(plt.cm.Blues(.2))
+    world.plot(column = 'pop_est',
+            cmap = 'Greens',
+            ax = ax,
+            legend = True)
+
+    plt.show()
